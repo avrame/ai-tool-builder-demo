2343c3e2e0fb825311efe1798bedca78a2c3c610
Avram Eisner <avrame@gmail.com>
2026-06-10 12:22:42 -0700

    feat: configure netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,38 @@
-import path from 'node:path'
-import { defineConfig } from 'vite'
+import path from "node:path";
+import { defineConfig } from "vite";
+import netlify from "@netlify/vite-plugin";
 
 const arrowPackages = [
-  '@arrow-js/core',
-  '@arrow-js/framework',
-  '@arrow-js/hydrate',
-  '@arrow-js/ssr',
-]
+  "@arrow-js/core",
+  "@arrow-js/framework",
+  "@arrow-js/hydrate",
+  "@arrow-js/ssr",
+];
 
 export default defineConfig(({ isSsrBuild }) => ({
   server: {
-    host: '127.0.0.1',
+    host: "127.0.0.1",
     port: 5173,
   },
   optimizeDeps: {
     exclude: arrowPackages,
   },
   ssr: {
-    noExternal: arrowPackages,
+    external: ["jsdom"],
   },
+  plugins: [netlify()],
   build: {
-    outDir: isSsrBuild ? 'dist/server' : 'dist/client',
+    outDir: isSsrBuild ? "dist/server" : "dist/client",
     emptyOutDir: !isSsrBuild,
     rollupOptions: {
       input: isSsrBuild
-        ? path.resolve(__dirname, 'src/entry-server.ts')
-        : path.resolve(__dirname, 'index.html'),
+        ? path.resolve(__dirname, "src/entry-server.ts")
+        : path.resolve(__dirname, "index.html"),
       output: isSsrBuild
         ? {
-            entryFileNames: 'entry-server.js',
+            entryFileNames: "entry-server.js",
           }
         : undefined,
     },
   },
-}))
+}));
